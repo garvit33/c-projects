d713fe94808c2ad9e488ddb631fac8a27a401359
garvit33 <125457089+garvit33@users.noreply.github.com>
2025-09-18 02:47:40 +0530

    Add files via upload

---
+++ numberguessinggame.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h> 
+int main() {
+    int random,guess;
+    int no_of_guess = 0;
+    srand(time(NULL));
+    printf ("welcome to number guessing game\n");
+    random = rand() % 100 + 1;
+
+    do {
+      printf ("\n please enter your number between 1 to 100: ");
+      scanf ("%d",&guess);
+      no_of_guess++;
+      
+      if  (guess < random) {
+           printf("guessr a larger number: \n");
+      }
+      else if (guess > random) {
+           printf ("guess a smaller number. \n");
+      }           
+      else {
+                         printf ("congrats you have guessed the number in %d attempts.\n ",no_of_guess);
+      }
+      
+      } while (guess != random);
+
+      printf ("hope you enjoyed the game thanks for playing :) \n ");
+      printf ("(developed by @garvit33 XD)");
+
+      return 0;
+
+}
